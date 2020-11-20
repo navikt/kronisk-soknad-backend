9bb4983b9f439764fb6f4abe7006713e8ec411c6
ehsansadeghi-inno <74188830+ehsansadeghi-inno@users.noreply.github.com>
2020-11-20 10:49:48 +0100

    lagt til validering til gravid request (#3)
    * lagt til validering til gravid request
    
    * Because Maja said so
    
    Co-authored-by: Ehsan Sadeghi <ehsans.sadeghi@nav.no>

--- src/main/kotlin/no/nav/helse/fritakagp/web/api/resreq/GravidResReq.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/web/api/resreq/GravidResReq.kt
@@ -1,12 +1,32 @@
 package no.nav.helse.fritakagp.web.api.resreq
 
+import no.nav.helse.arbeidsgiver.web.validation.isValidIdentitetsnummer
+import no.nav.helse.fritakagp.domain.Omplassering
+import no.nav.helse.fritakagp.web.dto.validation.isOmplasseringValgRiktig
+import no.nav.helse.fritakagp.web.dto.validation.isTiltakValid
+import org.valiktor.functions.isInIgnoringCase
+import org.valiktor.functions.isNotNull
+import org.valiktor.validate
 import java.time.LocalDate
 
 data class GravideSoknadRequest(
-    val dato: LocalDate,
-    val fnr: String,
-    val tilrettelegge: Boolean,
-    val tiltak: List<String>,
-    val tiltakBeskrivelse: String?,
-    val omplassering: String
-)
+        val dato: LocalDate,
+        val fnr: String,
+        val tilrettelegge: Boolean,
+        val tiltak: List<String>,
+        val tiltakBeskrivelse: String?,
+        val omplassering: String,
+        val omplasseringAarsak: String?
+) {
+    init {
+        validate(this) {
+            validate(GravideSoknadRequest::fnr).isValidIdentitetsnummer()
+            validate(GravideSoknadRequest::tiltak).isTiltakValid(it.tiltakBeskrivelse)
+            validate(GravideSoknadRequest::tilrettelegge).isNotNull()
+            validate(GravideSoknadRequest::omplassering).isInIgnoringCase(Omplassering.values().map { it -> it.name })
+            validate(GravideSoknadRequest::omplasseringAarsak).isOmplasseringValgRiktig(it.omplassering)
+
+        }
+
+    }
+}

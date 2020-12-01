02652126f30b7409d185a2c1573c00cde9584dad
tuxbear <ole-andre.riga.johansen@nav.no>
2020-12-01 09:56:55 +0100

    logger feil

--- src/main/kotlin/no/nav/helse/fritakagp/web/api/FritakAGPRoute.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/web/api/FritakAGPRoute.kt
@@ -12,10 +12,13 @@ import no.nav.helse.fritakagp.domain.getTiltakValue
 import no.nav.helse.fritakagp.web.api.resreq.GravideSoknadRequest
 import no.nav.helse.fritakagp.web.hentIdentitetsnummerFraLoginToken
 import no.nav.helse.fritakagp.web.hentUtløpsdatoFraLoginToken
+import org.slf4j.LoggerFactory
 import java.sql.SQLException
 
 @KtorExperimentalAPI
 fun Route.fritakAGP(repo:Repository) {
+
+    val logger = LoggerFactory.getLogger("FritakAGP API")
 
     route("/api/v1") {
 
@@ -45,6 +48,7 @@ fun Route.fritakAGP(repo:Repository) {
                     // TODO: Opprette en bakgrunnsjobb som sender søknaden videre
                     call.respond(HttpStatusCode.OK)
                 } catch (ex: SQLException) {
+                    logger.error(ex)
                     call.respond(HttpStatusCode.UnprocessableEntity)
                 }
             }

8d040a3a7823750ad422267f584e3bb443e7073d
tuxbear <ole-andre.riga.johansen@nav.no>
2020-12-14 11:17:06 +0100

    Første forsøk på tokenx-klient

--- src/main/kotlin/no/nav/helse/fritakagp/koin/PreprodKoinProfile.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/koin/PreprodKoinProfile.kt
@@ -6,8 +6,8 @@ import io.ktor.util.*
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbService
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.PostgresBakgrunnsjobbRepository
-import no.nav.helse.arbeidsgiver.integrasjoner.RestStsClient
-import no.nav.helse.arbeidsgiver.integrasjoner.RestStsClientImpl
+import no.nav.helse.arbeidsgiver.integrasjoner.AccessTokenProvider
+import no.nav.helse.arbeidsgiver.integrasjoner.ServiceUserTokenXTokenProvider
 import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.DokarkivKlient
 import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.DokarkivKlientImpl
 import no.nav.helse.arbeidsgiver.integrasjoner.oppgave.OppgaveKlient
@@ -19,6 +19,13 @@ import no.nav.helse.fritakagp.db.PostgresGravidSoeknadRepository
 import no.nav.helse.fritakagp.db.createHikariConfig
 import no.nav.helse.fritakagp.processing.gravid.GravidSoeknadPDFGenerator
 import no.nav.helse.fritakagp.processing.gravid.SoeknadGravidProcessor
+import no.nav.helse.fritakagp.tokenx.DefaultOAuth2HttpClient
+import no.nav.helse.fritakagp.tokenx.TokenResolver
+import no.nav.helse.fritakagp.tokenx.TokenXClientPropertiesConfig
+import no.nav.security.token.support.client.core.oauth2.ClientCredentialsTokenClient
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.core.oauth2.OnBehalfOfTokenClient
+import no.nav.security.token.support.client.core.oauth2.TokenExchangeClient
 import org.koin.core.module.Module
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -39,18 +46,25 @@ fun preprodConfig(config: ApplicationConfig) = module {
     single { SoeknadGravidProcessor(get(), get(), get(), get(), GravidSoeknadPDFGenerator(), get()) }
 }
 
-
 fun Module.externalSystemClients(config: ApplicationConfig) {
-    single { object: RestStsClient {
-            // STS er ikke tilgjengelig i GCP, og vi bruker en proxy for å snakke med tjenester i FSS
-            override fun getOidcToken(): String {
-                return "dummy-token-for-proxy"
-            }
-        }
-    } bind RestStsClient::class
 
+    single {
+        val clientConfig = TokenXClientPropertiesConfig(config)
+        val tokenResolver = TokenResolver()
+        val oauthHttpClient = DefaultOAuth2HttpClient(get())
+        val accessTokenService = OAuth2AccessTokenService(
+            tokenResolver,
+            OnBehalfOfTokenClient(oauthHttpClient),
+            ClientCredentialsTokenClient(oauthHttpClient),
+            TokenExchangeClient(oauthHttpClient)
+        )
+
+        val tokenxConfig = clientConfig.clientConfig["tokenx"] ?: error("Fant ikke config i application.conf")
+        ServiceUserTokenXTokenProvider(accessTokenService, tokenxConfig)
+    } bind AccessTokenProvider::class
 
     single { PdlClientImpl(config.getString("pdl_url"), get(), get(), get()) } bind PdlClient::class
     single { DokarkivKlientImpl(config.getString("dokarkiv.base_url"), get(), get()) } bind DokarkivKlient::class
     single { OppgaveKlientImpl(config.getString("oppgavebehandling.url"), get(), get()) } bind OppgaveKlient::class
 }
+
---
+++ src/main/kotlin/no/nav/helse/fritakagp/tokenx/TokenXComponents.kt
@@ -0,0 +1,36 @@
+package no.nav.helse.fritakagp.tokenx
+
+import io.ktor.client.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
+import no.nav.security.token.support.client.core.context.JwtBearerTokenResolver
+import no.nav.security.token.support.client.core.http.OAuth2HttpClient
+import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
+import no.nav.security.token.support.ktor.TokenValidationContextPrincipal
+import java.util.*
+
+
+class DefaultOAuth2HttpClient(private val httpClient: HttpClient) : OAuth2HttpClient {
+    override fun post(oAuth2HttpRequest: OAuth2HttpRequest): OAuth2AccessTokenResponse {
+        return runBlocking {
+            httpClient.submitForm(
+                url = oAuth2HttpRequest.tokenEndpointUrl.toString(),
+                formParameters = Parameters.build {
+                    oAuth2HttpRequest.formParameters.forEach {
+                        append(it.key, it.value)
+                    }
+                }
+            )
+        }
+    }
+}
+
+class TokenResolver: JwtBearerTokenResolver {
+    var tokenPrincipal: TokenValidationContextPrincipal? = null
+
+    override fun token(): Optional<String> {
+        return tokenPrincipal?.context?.firstValidToken?.map { it.tokenAsString } ?: Optional.empty()
+    }
+}

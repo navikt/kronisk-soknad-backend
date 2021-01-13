e82f4069c751bbc065b3b23fd2a0e38d5940650e
tuxbear <ole-andre.riga.johansen@nav.no>
2021-01-13 15:08:52 +0100

    Legger til endepunkt for altinnrettigheter

--- src/main/kotlin/no/nav/helse/fritakagp/koin/KoinProfiles.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/koin/KoinProfiles.kt
@@ -9,11 +9,13 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.json.*
 import io.ktor.config.*
 import io.ktor.util.*
+import no.nav.helse.arbeidsgiver.integrasjoner.altinn.AltinnOrganisasjon
 import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.DokarkivKlient
 import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.JournalpostRequest
 import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.JournalpostResponse
@@ -22,6 +24,8 @@ import no.nav.helse.arbeidsgiver.integrasjoner.oppgave.OpprettOppgaveRequest
 import no.nav.helse.arbeidsgiver.integrasjoner.oppgave.OpprettOppgaveResponse
 import no.nav.helse.arbeidsgiver.integrasjoner.pdl.*
 import no.nav.helse.arbeidsgiver.kubernetes.KubernetesProbeManager
+import no.nav.helse.arbeidsgiver.utils.loadFromResources
+import no.nav.helse.arbeidsgiver.web.auth.AltinnOrganisationsRepository
 import no.nav.helse.fritakagp.integration.gcp.BucketStorage
 import no.nav.helse.fritakagp.integration.gcp.MockBucketStorage
 import no.nav.helse.fritakagp.integration.virusscan.MockVirusScanner
@@ -81,58 +85,6 @@ val common = module {
 
 }
 
-fun Module.mockExternalDependecies() {
-    single { object: DokarkivKlient {
-        override fun journalførDokument(
-            journalpost: JournalpostRequest,
-            forsoekFerdigstill: Boolean,
-            callId: String
-        ): JournalpostResponse {
-            return JournalpostResponse("arkiv-ref", true, "J", null, emptyList())
-        }
-    } } bind DokarkivKlient::class
-
-    single {object: PdlClient {
-        override fun fullPerson(ident: String) =
-            PdlHentFullPerson(
-                PdlHentFullPerson.PdlFullPersonliste(
-                    emptyList(),
-                    emptyList(),
-                    emptyList(),
-                    emptyList(),
-                    emptyList()
-                ),
-
-                PdlHentFullPerson.PdlIdentResponse(listOf(PdlIdent("aktør-id", PdlIdent.PdlIdentGruppe.AKTORID))),
-
-                PdlHentFullPerson.PdlGeografiskTilknytning(
-                    PdlHentFullPerson.PdlGeografiskTilknytning.PdlGtType.UTLAND,
-                    null,
-                    null,
-                    "SWE"
-                )
-            )
-
-        override fun personNavn(ident: String) =
-            PdlHentPersonNavn.PdlPersonNavneliste(listOf(
-                PdlHentPersonNavn.PdlPersonNavneliste.PdlPersonNavn("Ola", "M", "Avsender", PdlPersonNavnMetadata("freg"))))
-    }
-
-    } bind PdlClient::class
-
-    single { object: OppgaveKlient {
-        override suspend fun opprettOppgave(
-            opprettOppgaveRequest: OpprettOppgaveRequest,
-            callId: String
-        ): OpprettOppgaveResponse = OpprettOppgaveResponse(1234)
-    } } bind OppgaveKlient::class
-
-    single { MockVirusScanner() } bind VirusScanner::class
-    single { MockBucketStorage() } bind BucketStorage::class
-}
-
-
-
 // utils
 @KtorExperimentalAPI
 fun ApplicationConfig.getString(path: String): String {
@@ -141,18 +93,20 @@ fun ApplicationConfig.getString(path: String): String {
 
 @KtorExperimentalAPI
 fun ApplicationConfig.getjdbcUrlFromProperties(): String {
-    return String.format("jdbc:postgresql://%s:%s/%s",
-            this.property("database.host").getString(),
-            this.property("database.port").getString(),
-            this.property("database.name").getString())
+    return String.format(
+        "jdbc:postgresql://%s:%s/%s",
+        this.property("database.host").getString(),
+        this.property("database.port").getString(),
+        this.property("database.name").getString()
+    )
 }
 
 
 inline fun <reified T : Any> Koin.getAllOfType(): Collection<T> =
-        let { koin ->
-            koin.rootScope.beanRegistry
-                    .getAllDefinitions()
-                    .filter { it.kind == Kind.Single }
-                    .map { koin.get<Any>(clazz = it.primaryType, qualifier = null, parameters = null) }
-                    .filterIsInstance<T>()
-        }
+    let { koin ->
+        koin.rootScope.beanRegistry
+            .getAllDefinitions()
+            .filter { it.kind == Kind.Single }
+            .map { koin.get<Any>(clazz = it.primaryType, qualifier = null, parameters = null) }
+            .filterIsInstance<T>()
+    }
---
+++ src/main/kotlin/no/nav/helse/fritakagp/web/api/AltinnRettigheterRoute.kt
@@ -0,0 +1,28 @@
+package no.nav.helse.fritakagp.web.api
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.util.*
+import no.nav.helse.arbeidsgiver.integrasjoner.altinn.AltinnBrukteForLangTidException
+import no.nav.helse.arbeidsgiver.web.auth.AltinnOrganisationsRepository
+import no.nav.helse.fritakagp.web.hentIdentitetsnummerFraLoginToken
+
+@KtorExperimentalAPI
+fun Route.altinnRoutes(authRepo: AltinnOrganisationsRepository) {
+
+    route("/api/v1") {
+        route("/arbeidsgivere") {
+            get("/") {
+                val id = hentIdentitetsnummerFraLoginToken(application.environment.config, call.request)
+                try {
+                    val rettigheter = authRepo.hentOrgMedRettigheterForPerson(id)
+                    call.respond(rettigheter)
+                } catch (ae: AltinnBrukteForLangTidException) {
+                    call.respond(HttpStatusCode.ExpectationFailed, "Altinn brukte for lang tid på å svare, prøv igjen om litt")
+                }
+            }
+        }
+    }
+}

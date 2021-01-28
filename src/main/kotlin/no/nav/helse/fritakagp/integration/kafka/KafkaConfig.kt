2541142a62ce44031161018a1dc37a32c79eb9ab
tuxbear <ole-andre.riga.johansen@nav.no>
2021-01-28 12:12:29 +0100

    Påkrevde miljøvariabler

--- src/main/kotlin/no/nav/helse/fritakagp/integration/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/integration/kafka/KafkaConfig.kt
@@ -5,18 +5,15 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.security.auth.SecurityProtocol
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
-import java.time.Duration
 
 private const val JAVA_KEYSTORE = "jks"
 private const val PKCS12 = "PKCS12"
 
-private fun envOrEmpty(envVar: String) = System.getenv()[envVar] ?: ""
-
+private fun envOrThrow(envVar: String) = System.getenv()[envVar] ?: throw IllegalStateException("$envVar er påkrevd miljøvariabel")
 
 fun producerConfig() = mutableMapOf<String, Any>(
-    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to envOrEmpty("KAFKA_BROKERS"),
+    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to envOrThrow("KAFKA_BROKERS"),
     ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,
     ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,
     ProducerConfig.ACKS_CONFIG to "all",
@@ -25,12 +22,13 @@ fun producerConfig() = mutableMapOf<String, Any>(
     SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "", //Disable server host name verification
     SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to JAVA_KEYSTORE,
     SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to PKCS12,
-    SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to envOrEmpty("KAFKA_TRUSTSTORE_PATH"),
-    SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to envOrEmpty("KAFKA_CREDSTORE_PASSWORD"),
-    SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to envOrEmpty("KAFKA_KEYSTORE_PATH"),
-    SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to envOrEmpty("KAFKA_CREDSTORE_PASSWORD"),
-    SslConfigs.SSL_KEY_PASSWORD_CONFIG to envOrEmpty("KAFKA_CREDSTORE_PASSWORD")
+    SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to envOrThrow("KAFKA_TRUSTSTORE_PATH"),
+    SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to envOrThrow("KAFKA_CREDSTORE_PASSWORD"),
+    SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to envOrThrow("KAFKA_KEYSTORE_PATH"),
+    SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to envOrThrow("KAFKA_CREDSTORE_PASSWORD"),
+    SslConfigs.SSL_KEY_PASSWORD_CONFIG to envOrThrow("KAFKA_CREDSTORE_PASSWORD")
 )
+
 fun producerLocalConfig() = mutableMapOf<String, Any>(
     ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
     ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,

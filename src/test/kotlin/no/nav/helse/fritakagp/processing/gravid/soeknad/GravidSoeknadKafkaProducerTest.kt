2270a54efb7c6f6ce03c002de0c3c1f45d8c61dc
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-02-09 09:42:01 +0100

    tester til mockk kafkaproducer

--- src/test/kotlin/no/nav/helse/fritakagp/processing/gravid/soeknad/GravidSoeknadKafkaProducerTest.kt
+++ src/test/kotlin/no/nav/helse/fritakagp/processing/gravid/soeknad/GravidSoeknadKafkaProducerTest.kt
@@ -1,6 +1,8 @@
 package no.nav.helse.fritakagp.processing.gravid.soeknad
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -10,22 +12,41 @@ import no.nav.helse.fritakagp.integration.kafka.SoeknadmeldingKafkaProducer
 import no.nav.helse.fritakagp.integration.kafka.SoeknadmeldingKafkaProducerProvider
 import no.nav.helse.fritakagp.integration.kafka.producerLocalSaslConfigWrongAuth
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.errors.AuthenticationException
+import org.junit.Assert
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
+import java.util.concurrent.TimeUnit
 
 class GravidSoeknadKafkaProducerTest {
-    val omMock = mockk<ObjectMapper>()
+    val omMock = ObjectMapper().registerModules(KotlinModule(),JavaTimeModule())
     val producerProviderMock = mockk<KafkaProducerProvider>()
     val producerMock = mockk<KafkaProducer<String, String>>()
 
+    @BeforeEach
+    fun setUp() {
+
+
+
+    }
     @Test
     internal fun  `sdf`() {
         every { producerProviderMock.createProducer(any()) } returns producerMock
-        val soeknadmelding = SoeknadmeldingKafkaProducer(producerLocalSaslConfigWrongAuth() as MutableMap<String, Any>, "test", omMock, producerProviderMock)
-        soeknadmelding.sendMessage(GravidTestData.soeknadGravid)
         every { producerMock.send(any()) } throws AuthenticationException("feil bruker/passord")
+        val soeknadmelding = SoeknadmeldingKafkaProducer(producerLocalSaslConfigWrongAuth() as MutableMap<String, Any>, "test", omMock, producerProviderMock)
+        try {
+            soeknadmelding.sendMessage(GravidTestData.soeknadGravid)
+            Assert.fail("feil bruker/passord")
+        } catch (ex: ExecutionException) {
+            print(ex.message)
+        }
+        every { producerMock.send(any()).get(10, TimeUnit.SECONDS) } returns RecordMetadata(null,0,0,0,0,0,0)
         soeknadmelding.sendMessage(GravidTestData.soeknadGravid)
-        verify { producerProviderMock.createProducer(any()) }
+        verify(exactly = 2) { producerProviderMock.createProducer(any()) }
         //verify { producerMock.send(any()) }
 
     }

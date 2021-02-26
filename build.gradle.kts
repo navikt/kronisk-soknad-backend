28c27ff85b65b5b78486fd9dcefd1c12077dc5eb
sorvoja <trond.anders.sorvoja@nav.no>
2021-02-26 14:41:04 +0100

    ny versjon av felles backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
 
     implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.02.10-13-53-bb1ff")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.02.26-14-35-02259")
     implementation("no.nav.common:log:2.2020.10.15_11.43-b1f02e7bd6ae")
 
     implementation(kotlin("stdlib"))

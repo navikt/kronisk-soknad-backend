e05c4cafc14703177316499f5f75a5ac81604b83
tuxbear <ole-andre.riga.johansen@nav.no>
2021-03-02 15:23:08 +0100

    POH-682: Ny fellesbib med charencodig i request til dokmot

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
 
     implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.02.26-15-09-39c2c")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.03.02-15-19-eb0ee")
     implementation("no.nav.common:log:2.2020.10.15_11.43-b1f02e7bd6ae")
 
     implementation(kotlin("stdlib"))

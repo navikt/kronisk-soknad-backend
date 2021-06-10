da3de18878c42918260ad804f9e07ac2a6bcd0ad
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-06-10 09:50:49 +0200

    fjernet acceptableCOntentTypes html/text

--- src/main/kotlin/no/nav/helse/fritakagp/koin/KoinProfiles.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/koin/KoinProfiles.kt
@@ -79,7 +79,6 @@ val common = module {
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
             }
-            acceptContentTypes += ContentType("application", "text/html")
         }
     }
 

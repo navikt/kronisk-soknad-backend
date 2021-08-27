c7b0ef45c90af08546573b1833cf5181bf5c3e16
sorvoja <trond.anders.sorvoja@nav.no>
2021-08-27 15:03:34 +0200

    snyk oppdateringer

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
         }
         implementation("io.netty:netty-codec-http2") {
             version {
-                strictly("4.1.59.Final")
+                strictly("4.1.60.Final")
             }
             because("snyk control")
         }
@@ -81,15 +81,15 @@ dependencies {
         }
         implementation("io.netty:netty-transport-native-epoll") {
             version {
-                strictly("4.1.59.Final")
+                strictly("4.1.60.Final")
             }
             because("snyk control")
         }
 
     }
     implementation("commons-codec:commons-codec:1.13") // overstyrer transiente 1.10
-    implementation("io.netty:netty-codec:4.1.59.Final") // overstyrer transiente 4.1.44
-    implementation("io.netty:netty-codec-http:4.1.59.Final") // overstyrer transiente 4.1.51.Final gjennom ktor-server-netty
+    implementation("io.netty:netty-codec:4.1.60.Final") // overstyrer transiente 4.1.44
+    implementation("io.netty:netty-codec-http:4.1.60.Final") // overstyrer transiente 4.1.51.Final gjennom ktor-server-netty
     implementation("org.eclipse.jetty:jetty-server:9.4.41.v20210516")
     implementation("com.google.guava:guava:30.0-jre") //[Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415] overstyrer versjon 29.0
     // -- end snyk fixes

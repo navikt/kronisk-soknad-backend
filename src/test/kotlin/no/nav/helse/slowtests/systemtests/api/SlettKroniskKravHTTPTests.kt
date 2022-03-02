56928b185c11dcb1c1a42d7574dc305b20cd7263
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-03-02 09:41:47 +0100

    Prøver å fikse slett kronisk krav tester

--- src/test/kotlin/no/nav/helse/slowtests/systemtests/api/SlettKroniskKravHTTPTests.kt
+++ src/test/kotlin/no/nav/helse/slowtests/systemtests/api/SlettKroniskKravHTTPTests.kt
@@ -1,74 +1,37 @@
 package no.nav.helse.slowtests.systemtests.api
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import io.ktor.client.call.*
 import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.mockk
 import no.nav.helse.GravidTestData
 import no.nav.helse.KroniskTestData
-import no.nav.helse.arbeidsgiver.bakgrunnsjobb.Bakgrunnsjobb
-import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
-import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.DokarkivKlient
-import no.nav.helse.arbeidsgiver.integrasjoner.dokarkiv.JournalpostResponse
-import no.nav.helse.arbeidsgiver.integrasjoner.oppgave.OppgaveKlient
-import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlClient
-import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlHentFullPerson
-import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlHentPersonNavn
-import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlIdent
-import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlPersonNavnMetadata
-import no.nav.helse.fritakagp.db.GravidKravRepository
 import no.nav.helse.fritakagp.db.KroniskKravRepository
-import no.nav.helse.fritakagp.domain.Arbeidsgiverperiode
 import no.nav.helse.fritakagp.domain.GravidKrav
-import no.nav.helse.fritakagp.integration.brreg.BrregClient
-import no.nav.helse.fritakagp.integration.gcp.BucketStorage
-import no.nav.helse.fritakagp.processing.gravid.krav.GravidKravPDFGenerator
-import no.nav.helse.fritakagp.processing.gravid.krav.GravidKravProcessor
-import no.nav.helse.fritakagp.service.BehandlendeEnhetService
-import no.nav.helse.fritakagp.web.api.resreq.ValidationProblem
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.koin.test.inject
-import java.time.LocalDate
 
 class SlettKroniskKravHTTPTests : SystemTestBase() {
     private val kravKroniskUrl = "/api/v1/kronisk/krav"
 
     @Test
-    internal fun `Returnerer kravet når korrekt bruker er innlogget, 404 når ikke`() = suspendableTest {
+    internal fun `Skal returnere 200 OK når vi sletter med korrekt bruker innlogget`() = suspendableTest {
         val repo by inject<KroniskKravRepository>()
 
         repo.insert(KroniskTestData.kroniskKrav)
 
-        val exception = assertThrows<ClientRequestException>
-        {
-            httpClient.delete<HttpResponse> {
-                appUrl("$kravKroniskUrl/${KroniskTestData.kroniskKrav.id}")
-                contentType(ContentType.Application.Json)
-                loggedInAs("123456789")
-            }
-        }
-
-        Assertions.assertThat(exception.response.status).isEqualTo(HttpStatusCode.NotFound)
-
-        val accessGrantedForm = httpClient.get<GravidKrav> {
+        val response = httpClient.delete<HttpResponse> {
             appUrl("$kravKroniskUrl/${KroniskTestData.kroniskKrav.id}")
             contentType(ContentType.Application.Json)
             loggedInAs(KroniskTestData.kroniskKrav.identitetsnummer)
         }
 
-        Assertions.assertThat(accessGrantedForm).isEqualTo(GravidTestData.gravidKrav)
+        assertThat(response.status).isEqualTo(HttpStatusCode.OK)
     }
+
     @Test
     fun `Skal returnere forbidden hvis virksomheten ikke er i auth listen fra altinn`() = suspendableTest {
         val repo by inject<KroniskKravRepository>()
@@ -76,11 +39,11 @@ class SlettKroniskKravHTTPTests : SystemTestBase() {
         repo.insert(KroniskTestData.kroniskKrav)
         val responseExcepion = assertThrows<ClientRequestException> {
             httpClient.delete<HttpResponse> {
-                appUrl("$kravKroniskUrl/${GravidTestData.gravidKrav.id}")
+                appUrl("$kravKroniskUrl/${KroniskTestData.kroniskKrav.id}")
                 contentType(ContentType.Application.Json)
                 loggedInAs("123456789")
             }
         }
-        Assertions.assertThat(responseExcepion.response.status).isEqualTo(HttpStatusCode.Forbidden)
+        assertThat(responseExcepion.response.status).isEqualTo(HttpStatusCode.Forbidden)
     }
 }

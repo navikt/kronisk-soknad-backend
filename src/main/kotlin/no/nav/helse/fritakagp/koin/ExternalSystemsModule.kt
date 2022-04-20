6c393b695372b2cc30fdd7e5051ae87db9e1582f
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-04-20 14:32:15 +0200

    Try app id in scope

--- src/main/kotlin/no/nav/helse/fritakagp/koin/ExternalSystemsModule.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/koin/ExternalSystemsModule.kt
@@ -88,7 +88,7 @@ fun Module.externalSystemClients(config: ApplicationConfig) {
     } bind AccessTokenProvider::class
 
     single(named("ARBEIDSGIVERNOTIFIKASJON")) {
-        val clientConfig = OAuth2ClientPropertiesConfig(config, "api://dev-gcp.fager.notifikasjon-produsent-api/.default")
+        val clientConfig = OAuth2ClientPropertiesConfig(config, "7e5c11cd-f49c-43b9-880d-e733fb974261")
         val tokenResolver = TokenResolver()
         val oauthHttpClient = DefaultOAuth2HttpClient(get())
         val accessTokenService = OAuth2AccessTokenService(

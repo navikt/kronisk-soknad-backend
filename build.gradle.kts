5becc4867ea080c060a9c6f70d0b2a7b7d1f7da6
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-04-29 11:42:01 +0200

    Oppdaterer felles backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("com.github.navikt:brukernotifikasjon-schemas:$brukernotifikasjonSchemasVersion")
     implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2022.04.29-10-24-ddb8a")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2022.04.29-11-39-bcd49")
     implementation("no.nav.common:log:2.2020.10.27_15.17-901cc4cfbbe4")
     implementation(kotlin("stdlib"))
     implementation("org.slf4j:slf4j-api:1.7.30")

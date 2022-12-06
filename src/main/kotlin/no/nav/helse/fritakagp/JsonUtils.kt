8634fff0c36dc2c3ce71d4cc6a909f106603b468
Mikael Bjerga <mikael.bjerga@nav.no>
2022-12-06 17:37:12 +0100

    Fjern duplisert opprettelse av object mapper

--- src/main/kotlin/no/nav/helse/fritakagp/JsonUtils.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/JsonUtils.kt
@@ -8,22 +8,27 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
-fun customObjectMapper(): ObjectMapper =
-    ObjectMapper().apply {
-        registerModule(KotlinModule())
-        registerModule(Jdk8Module())
-        registerModule(JavaTimeModule())
-        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        configure(SerializationFeature.INDENT_OUTPUT, true)
-        configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-
-        setDefaultPrettyPrinter(
-            DefaultPrettyPrinter().apply {
-                indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
-                indentObjectsWith(DefaultIndenter("  ", "\n"))
-            }
+fun customObjectMapper(customPrettyPrinter: Boolean = true): ObjectMapper =
+    jacksonObjectMapper().apply {
+        registerModules(
+            Jdk8Module(),
+            JavaTimeModule()
         )
+
+        enable(SerializationFeature.INDENT_OUTPUT)
+        enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+
+        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+
+        if (customPrettyPrinter) {
+            setDefaultPrettyPrinter(
+                DefaultPrettyPrinter().apply {
+                    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                    indentObjectsWith(DefaultIndenter("  ", "\n"))
+                }
+            )
+        }
     }

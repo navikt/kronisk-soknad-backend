7e310e2d3d57b4df7bd37f99c71effdfc2694aed
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2023-03-01 13:43:08 +0100

    Sett aktivdato og frist til orginal dato (#243)

--- src/main/kotlin/no/nav/helse/fritakagp/processing/gravid/krav/OpprettRobotOppgaveGravidProcessor.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/processing/gravid/krav/OpprettRobotOppgaveGravidProcessor.kt
@@ -84,8 +84,8 @@ class OpprettRobotOppgaveGravidProcessor(
             behandlingstype = digitalKravBehandingsType,
             oppgavetype = oppgaveType,
             behandlingstema = fritakAGPBehandingsTema,
-            aktivDato = LocalDate.now(),
-            fristFerdigstillelse = LocalDate.now().plusDays(7),
+            aktivDato = krav.opprettet.toLocalDate(),
+            fristFerdigstillelse = krav.opprettet.toLocalDate().plusDays(7),
             prioritet = "NORM"
         )
 

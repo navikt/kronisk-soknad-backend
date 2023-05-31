a75e3b78ec3a04c052138f90995768453f4b67ef
Alexander Petrov <alexander.petrov@nav.no>
2023-05-31 10:59:55 +0200

    over16dager validering co-authored Gustav Bergren<gustav.bergren@nav.no>

--- src/main/kotlin/no/nav/helse/fritakagp/web/api/resreq/validation/CustomValidatorConstraints.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/web/api/resreq/validation/CustomValidatorConstraints.kt
@@ -2,6 +2,7 @@ package no.nav.helse.fritakagp.web.api.resreq.validation
 
 import no.nav.helse.fritakagp.domain.AgpFelter
 import no.nav.helse.fritakagp.domain.Arbeidsgiverperiode
+import no.nav.helse.fritakagp.domain.ArbeidsgiverperiodeNy
 import no.nav.helse.fritakagp.domain.FravaerData
 import no.nav.helse.fritakagp.domain.GodkjenteFiletyper
 import no.nav.helse.fritakagp.domain.KroniskSoeknad
@@ -10,6 +11,7 @@ import org.valiktor.Constraint
 import org.valiktor.Validator
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import kotlin.math.abs
 
 interface CustomConstraint : Constraint {
     override val messageBundle: String
@@ -25,38 +27,63 @@ fun <E> Validator<E>.Property<Iterable<Periode>?>.refusjonsDagerIkkeOverstigerPe
     }
 
 class MåVæreVirksomhetContraint : CustomConstraint
+
 fun <E> Validator<E>.Property<String?>.isVirksomhet(erVirksomhet: Boolean) =
     this.validate(MåVæreVirksomhetContraint()) { erVirksomhet }
 
+fun <E> Validator<E>.Property<List<ArbeidsgiverperiodeNy>?>.oppholdOverstiger16dager() =
+    this.validate(MåVæreVirksomhetContraint()) {
+        val sortedArbeidsgiverperiodeNy: List<ArbeidsgiverperiodeNy> = it!!.sortedBy {
+            it.perioder?.minOf {
+                it.fom
+            }
+        }.toList()
+        val validationResult =
+            sortedArbeidsgiverperiodeNy.zipWithNext { a, b ->
+                Pair(a.tom, b.fom)
+            }.map {
+                abs(ChronoUnit.DAYS.between(it.first, it.second))
+            }.all {
+                it > 16
+            }
+        return@validate validationResult
+    }
+
 class FraDatoKanIkkeKommeEtterTomDato : CustomConstraint
+
 fun <E> Validator<E>.Property<LocalDate?>.datoerHarRiktigRekkefolge(tom: LocalDate) =
     this.validate(FraDatoKanIkkeKommeEtterTomDato()) { fom -> fom!!.isEqual(tom) || fom!!.isBefore(tom) }
 
 class MaanedsInntektErStorreEnTiMil : CustomConstraint
+
 fun <E> Validator<E>.Property<Double?>.maanedsInntektErMellomNullOgTiMil() =
     this.validate(MaanedsInntektErStorreEnTiMil()) {
         it!! > 0.0 && it!! <= TiMil
     }
 
 class AntallPerioderErMellomNullOgTreHundre() : CustomConstraint
+
 fun <E> Validator<E>.Property<Iterable<KroniskSoeknad>?>.antallPerioderErMellomNullOgTreHundre() =
     this.validate(AntallPerioderErMellomNullOgTreHundre()) { ps ->
         ps!!.any { p -> p.antallPerioder >= 1 && p.antallPerioder < 300 }
     }
 
 class DataUrlExtensionConstraints : CustomConstraint
+
 fun <E> Validator<E>.Property<String?>.isGodskjentFiletyper() =
     this.validate(DataUrlExtensionConstraints()) {
         return@validate enumContains<GodkjenteFiletyper>(extractFilExtDel(it!!.uppercase()))
     }
 
 class DataUrlBase64Constraints : CustomConstraint
+
 fun <E> Validator<E>.Property<String?>.isAvStorrelse(minSize: Long, maxSize: Long) =
     this.validate(DataUrlBase64Constraints()) {
         return@validate extractBase64Del(it!!).toByteArray().size <= maxSize && extractBase64Del(it!!).toByteArray().size > minSize
     }
 
 class MaxAgeFravaersDataConstraint : CustomConstraint
+
 fun <E> Validator<E>.Property<Iterable<FravaerData>?>.ingenDataEldreEnn(aar: Long) =
     this.validate(MaxAgeFravaersDataConstraint()) { ps ->
         val minDate = LocalDate.now().minusYears(aar).withDayOfMonth(1)
@@ -66,6 +93,7 @@ fun <E> Validator<E>.Property<Iterable<FravaerData>?>.ingenDataEldreEnn(aar: Lon
     }
 
 class NoFutureFravaersDataConstraint : CustomConstraint
+
 fun <E> Validator<E>.Property<Iterable<FravaerData>?>.ingenDataFraFremtiden() =
     this.validate(NoFutureFravaersDataConstraint()) { ps ->
         val maxDate = LocalDate.now().withDayOfMonth(1)
@@ -75,6 +103,7 @@ fun <E> Validator<E>.Property<Iterable<FravaerData>?>.ingenDataFraFremtiden() =
     }
 
 class MaxNumFravaersdagFravaersDataConstraint : CustomConstraint
+
 fun <E> Validator<E>.Property<Iterable<FravaerData>?>.ikkeFlereFravaersdagerEnnDagerIMaanden() =
     this.validate(MaxNumFravaersdagFravaersDataConstraint()) { ps ->
         return@validate !ps!!.any {

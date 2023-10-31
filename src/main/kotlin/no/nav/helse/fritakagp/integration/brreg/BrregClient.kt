15ada507b8706dd32cc05a13a3668bf0c92aeca2
Morten Byhring <morten.byhring@nav.no>
2023-10-31 14:26:40 +0100

    fikset brreg: sett contentType json i klient, tilpass ny klient til testene

--- src/main/kotlin/no/nav/helse/fritakagp/integration/brreg/BrregClient.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/integration/brreg/BrregClient.kt
@@ -4,6 +4,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
 
 interface BrregClient {
     suspend fun getVirksomhetsNavn(orgnr: String): String?
@@ -22,37 +26,40 @@ class MockBrregClient : BrregClient {
 
 class BrregClientImpl(private val httpClient: HttpClient, private val brregUndervirksomhetUrl: String) : BrregClient {
     override suspend fun getVirksomhetsNavn(orgnr: String): String? {
-        var navn: String? = null
-        try {
-            val url = "${brregUndervirksomhetUrl.trimEnd('/')}/$orgnr"
-            val underenhetResponse: UnderenheterResponse = httpClient.get(url).body()
-            val navn = underenhetResponse.navn
-        } catch (cause: Throwable) {
-            when (cause) {
-                is ClientRequestException -> {
-                    if (404 == cause.response?.status?.value) {
-                        navn = "Ukjent arbeidsgiver"
-                    }
-                }
-                else -> throw cause
+        var navn: String? = "Ukjent arbeidsgiver"
+        val httpResponse: HttpResponse = httpResponse(orgnr)
+        if (httpResponse.status.isSuccess()) {
+            val enhet: UnderenheterResponse = httpResponse.body()
+            navn = enhet.navn
+        } else {
+            if (404 == httpResponse.status?.value) {
+                return navn
             }
+            throw ClientRequestException(httpResponse, "Feil ved kall til brreg")
         }
         return navn
     }
 
+    private suspend fun httpResponse(orgnr: String): HttpResponse {
+        val url = "${brregUndervirksomhetUrl.trimEnd('/')}/$orgnr"
+        val httpResponse: HttpResponse = httpClient.get(url) {
+            contentType(ContentType.Application.Json)
+        }
+        return httpResponse
+    }
+
     // Sjekker at vi har en virksomhet som eksisterer og ikke er slettet.
     override suspend fun erVirksomhet(orgNr: String): Boolean {
-        return try {
-            val url = "${brregUndervirksomhetUrl.trimEnd('/')}/$orgNr"
-            val underenheterResponse: UnderenheterResponse = httpClient.get(url).body()
-            val slettedato = underenheterResponse.sletteDato
+        val httpResponse: HttpResponse = httpResponse(orgNr)
+        if (httpResponse.status.isSuccess()) {
+            val enhet: UnderenheterResponse = httpResponse.body()
+            val slettedato = enhet.sletteDato
             return (slettedato == null)
-        } catch (cause: ClientRequestException) {
-            if (404 == cause.response?.status?.value) {
-                false
-            } else {
-                throw cause
+        } else {
+            if (404 == httpResponse.status?.value) {
+                return false
             }
+            throw ClientRequestException(httpResponse, "Feil ved kall til brreg")
         }
     }
 }

14f0cb30bb19bc3fbba865f6330a92ba7104a2a5
Morten Byhring <morten.byhring@nav.no>
2023-11-24 09:14:15 +0100

    utvid parallelltest

--- src/test/kotlin/no/nav/helse/slowtests/ParallellBakgrunnsjobbServiceTest.kt
+++ src/test/kotlin/no/nav/helse/slowtests/ParallellBakgrunnsjobbServiceTest.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.runBlocking
 import no.nav.helse.KroniskTestData
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb2.Bakgrunnsjobb
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb2.BakgrunnsjobbRepository
+import no.nav.helse.arbeidsgiver.bakgrunnsjobb2.BakgrunnsjobbStatus
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb2.MockBakgrunnsjobbRepository
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb2.ParallellBakgrunnsjobbService
 import no.nav.helse.fritakagp.customObjectMapper
@@ -17,7 +18,10 @@ import no.nav.helse.fritakagp.db.GravidKravRepository
 import no.nav.helse.fritakagp.db.KroniskKravRepository
 import no.nav.helse.fritakagp.processing.arbeidsgivernotifikasjon.ArbeidsgiverOppdaterNotifikasjonProcessor
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.sql.SQLException
+import java.time.LocalDateTime
 import java.util.UUID
 
 class ParallellBakgrunnsjobbServiceTest {
@@ -34,6 +38,8 @@ class ParallellBakgrunnsjobbServiceTest {
             KroniskTestData.kroniskKrav
         }
 
+        every { gravidRepo.getById(any()) } throws SQLException()
+
         val service = ParallellBakgrunnsjobbService(repo, 1000)
         for (i in 1..1000) {
             repo.save(
@@ -43,7 +49,17 @@ class ParallellBakgrunnsjobbServiceTest {
                     data = """{"skjemaId": "$id", "skjemaType": "KroniskKrav"}"""
                 )
             )
+            if (i == 500) { // sleng inn en som feiler
+                repo.save(
+                    Bakgrunnsjobb(
+                        uuid = UUID.randomUUID(),
+                        type = "arbeidsgiveroppdaternotifikasjon",
+                        data = """{"skjemaId": "$id", "skjemaType": "GravidKrav"}"""
+                    )
+                )
+            }
         }
+
         service.registrer(ArbeidsgiverOppdaterNotifikasjonProcessor(gravidRepo, kroniskRepository, customObjectMapper(), klient))
         runBlocking {
             val job = coroutineScope {
@@ -67,5 +83,8 @@ class ParallellBakgrunnsjobbServiceTest {
             coVerify(exactly = 2000) { klient.nyStatusSakByGrupperingsid(any(), any(), any(), any()) }
             verify(exactly = 2000) { kroniskRepository.getById(any()) }
         }
+        // kjøretid for jobb oppdateres til *neste* kjøringstidspunkt også når den er kjørt OK, så legger på en dag for å hente ut alle:
+        assertEquals(2000, repo.findByKjoeretidBeforeAndStatusIn(LocalDateTime.now().plusDays(1), setOf(BakgrunnsjobbStatus.OK)).size)
+        assertEquals(1, repo.findByKjoeretidBeforeAndStatusIn(LocalDateTime.now().plusDays(1), setOf(BakgrunnsjobbStatus.FEILET)).size)
     }
 }

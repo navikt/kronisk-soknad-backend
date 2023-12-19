9f94d0cf01e67ea5d248fade18a28446918911da
Morten Byhring <morten.byhring@nav.no>
2023-12-19 16:07:13 +0100

    Duplikatsjekk på kroniskKrav
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>

---
+++ src/test/kotlin/no/nav/helse/fritakagp/domain/KroniskKravTest.kt
@@ -0,0 +1,69 @@
+package no.nav.helse.fritakagp.domain
+
+import no.nav.helse.KroniskTestData
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class KroniskKravTest {
+
+    @Test
+    fun `Duplikatsjekk - orginal skal være lik seg selv`() {
+        val krav = KroniskTestData.kroniskKrav
+        assertTrue(krav.isDuplicate(krav))
+    }
+
+    @Test
+    fun `Duplikatsjekk - forskjellige verdier i virksomhetsnummer gir ingen duplikat-treff`() {
+        val orginalKrav = KroniskTestData.kroniskKrav
+        val nyttKrav = orginalKrav.copy(virksomhetsnummer = "noe helt forskjellig")
+        assertFalse(orginalKrav.isDuplicate(nyttKrav))
+    }
+
+    @Test
+    fun `Duplikatsjekk - forskjellige verdier i identititetsnummer gir ingen duplikat-treff`() {
+        val orginalKrav = KroniskTestData.kroniskKrav
+        val nyttKrav = orginalKrav.copy(identitetsnummer = "noe helt forskjellig")
+        assertFalse(orginalKrav.isDuplicate(nyttKrav))
+    }
+
+    @Test
+    fun `Duplikatsjekk - forskjellige perioder i identititetsnummer gir ingen duplikat-treff`() {
+        val agp1 = Arbeidsgiverperiode(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 16), 10, 1000.0, 1.0)
+        val endretFraDato = agp1.copy(fom = agp1.fom.plusDays(1))
+        val endretTilDato = agp1.copy(tom = agp1.tom.plusDays(1))
+        val endretRefusjon = agp1.copy(antallDagerMedRefusjon = 11)
+        val endretInntekt = agp1.copy(månedsinntekt = 1001.0)
+        val endretGradering = agp1.copy(gradering = 0.8)
+        val perioder = listOf(agp1, endretFraDato, endretTilDato, endretRefusjon, endretInntekt, endretGradering)
+
+        perioder.allPairs().forEach {
+            assertFalse(it.first.equals(it.second))
+        }
+    }
+
+    @Test
+    fun `Duplikatsjekk - forskjellige verdier i kontrolldager gir ingen duplikat-treff`() {
+        val orginalKrav = KroniskTestData.kroniskKrav.copy(kontrollDager = 14)
+        val nyttKrav = orginalKrav.copy(kontrollDager = 3)
+        assertFalse(orginalKrav.isDuplicate(nyttKrav))
+    }
+
+    @Test
+    fun `Duplikatsjekk - forskjellige verdier i antallDager gir ingen duplikat-treff`() {
+        val orginalKrav = KroniskTestData.kroniskKrav.copy(antallDager = 1)
+        val nyttKrav = orginalKrav.copy(antallDager = 2)
+        assertFalse(orginalKrav.isDuplicate(nyttKrav))
+    }
+
+    fun <T> List<T>.allPairs(): List<Pair<T, T>> {
+        val pairs = mutableListOf<Pair<T, T>>()
+        for (i in 0 until size) {
+            for (j in i + 1 until size) {
+                pairs.add(this[i] to this[j])
+            }
+        }
+        return pairs
+    }
+}
--- src/test/kotlin/no/nav/helse/slowtests/systemtests/api/KroniskKravHTTPTests.kt
+++ src/test/kotlin/no/nav/helse/slowtests/systemtests/api/KroniskKravHTTPTests.kt
@@ -2,6 +2,7 @@ package no.nav.helse.slowtests.systemtests.api
 
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
@@ -151,5 +152,20 @@ class KroniskKravHTTPTests : SystemTestBase() {
         res.violations.forEach {
             assertThat(it.propertyPath).isIn(possiblePropertyPaths)
         }
+    }
+
+    @Test
+    internal fun `Oppdaterer ikke når ny request er duplikat`() = suspendableTest {
+        val repo by inject<KroniskKravRepository>()
+
+        val krav = repo.insert(KroniskTestData.kroniskKravRequestValid.toDomain("hohoho", "god", "jul"))
+
+        val response = httpClient.patch {
+            appUrl("$kravKroniskUrl/${krav.id}")
+            contentType(ContentType.Application.Json)
+            loggedInAs(KroniskTestData.kroniskKrav.identitetsnummer)
+            setBody(KroniskTestData.kroniskKravRequestValid)
+        }
+        assertThat(response.status).isEqualTo(HttpStatusCode.Conflict)
     }
 }

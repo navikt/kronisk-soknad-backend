38d188608b042bf3201d05aa8a6d56bd5914ba07
Morten Byhring <morten.byhring@nav.no>
2024-02-16 15:42:52 +0100

    fjerner ubrukt config og test

--- src/main/kotlin/no/nav/helse/fritakagp/integration/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/integration/kafka/KafkaConfig.kt
@@ -4,30 +4,14 @@ import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializer
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.security.auth.SecurityProtocol
-import org.apache.kafka.common.serialization.StringSerializer
 
 private const val JAVA_KEYSTORE = "jks"
 private const val PKCS12 = "PKCS12"
-private const val LOCALHOST = "localhost:9092"
-private const val GROUP_ID_CONFIG = "helsearbeidsgiver-fritakagp"
 
 private fun envOrThrow(envVar: String) = System.getenv()[envVar] ?: throw IllegalStateException("$envVar er påkrevd miljøvariabel")
-
-fun kravmeldingKafkaProps() =
-    mapOf(
-        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
-    ) + gcpCommonKafkaProps()
-
-fun soeknadmeldingKafkaProps() =
-    mapOf(
-        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
-    ) + gcpCommonKafkaProps()
 
 fun brukernotifikasjonKafkaProps() =
     mapOf(
@@ -52,17 +36,3 @@ fun gcpCommonKafkaProps() = mapOf<String, Any>(
     SslConfigs.SSL_KEY_PASSWORD_CONFIG to envOrThrow("KAFKA_CREDSTORE_PASSWORD")
 )
 
-fun localCommonKafkaProps() = mutableMapOf<String, Any>(
-    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to LOCALHOST,
-    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,
-    ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,
-    ProducerConfig.ACKS_CONFIG to "1"
-)
-
-fun consumerFakeConfig() = mutableMapOf<String, Any>(
-    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to LOCALHOST,
-    ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG to "30000",
-    ConsumerConfig.GROUP_ID_CONFIG to GROUP_ID_CONFIG,
-    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
-    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest"
-)

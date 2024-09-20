1bd2d74b3104e31c6637c2837e6e994137487ba8
Morten Byhring <morten.byhring@nav.no>
2024-09-20 15:29:51 +0200

    samlet kotlinVersjon ett sted, endret på deprecated jvmTarget. Bumpet apache cfx og logback

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
         val dependencyAnalysisVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
         id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
         id("com.github.ben-manes.versions") version versionUpdatesVersion
         id("com.autonomousapps.dependency-analysis") version dependencyAnalysisVersion

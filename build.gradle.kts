d95151bda0744ca910a766238358db0a9466c413
Mehdi.Zare <mehdi.zare@nav.no>
2024-12-18 13:43:28 +0100

    Wiremock dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -144,6 +144,7 @@ dependencies {
     val valiktorVersion: String by project
     val maskinportenClientVersion: String by project
     val tmsVarselKotlinBuilderVersion: String by project
+    val wiremockeVersion : String by project
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("ch.qos.logback:logback-classic:$logback_version")
@@ -222,7 +223,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testImplementation("org.wiremock:wiremock:3.10.0")
+    testImplementation("org.wiremock:wiremock:$wiremockeVersion")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
